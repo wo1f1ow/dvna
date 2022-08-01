de736481364f77014bd309574ae8b6031d897093
laur <57439610+wo1f1ow@users.noreply.github.com>
2022-08-01 09:39:31 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+# Docker entrypoint is a Dockerfile directive or 
+# instruction that is used to specify the executable which should run when a container is started from a Docker image.
 chmod +x /app/wait-for-it.sh
 
 /bin/bash /app/wait-for-it.sh $MYSQL_HOST:$MYSQL_PORT -t 300 -- npm start
